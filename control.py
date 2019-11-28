ff98147a14f49f84690b04e34c9638ce377d8836
L.M. Rojas Aguilera <rojas.luis.mail@gmail.com>
2019-11-27 23:17:39 -0400

    Cheking OUNoise outputs. Adapting actions formatting

--- control.py
+++ control.py
@@ -36,3 +36,43 @@ def on_game(frame, el_coord, el_img, threshold=4):
     #cv2.imshow('el', el)
     dif = np.array(cut - el).flatten()
     return sum(dif) > threshold
+
+def transform_action(action, action_range, action_low):
+    return action * action_range + action_low
+
+if __name__ == "__main__":
+    from ddpg import OUNoise
+    from asb import AndroidScreenBuffer
+    from matplotlib import pyplot as plt
+    #buff = AndroidScreenBuffer()
+    #h, w = buff.get_device_screen_shape()
+    exploration_mu = 0
+    exploration_theta = 0.15
+    exploration_sigma = 0.2
+    action_size = 3
+    action_low = np.array([1, 0, 1])
+    action_high = np.array([10, 359, 2000])
+    action_range = action_high - action_low
+
+    action = np.array([np.random.uniform() for _ in action_low])
+    noise = OUNoise(action.shape[0], exploration_mu,
+                                 exploration_theta, exploration_sigma)
+    values = np.zeros((100, 3))
+    iis = [i for i in range(100)]
+    for i in iis:
+        action = action + noise.sample()
+        action = np.array(transform_action(action, action_range, action_low),
+                          dtype='uint8')
+        values[i] = action
+
+    fig, ax = plt.subplots(3, sharex='col', sharey='row')
+    for i in range(3):
+        ax[i].plot(iis, values[i])
+
+    plt.show()
+
+        #put(*action, device_width=w, device_height=h)
+
+
+
+
