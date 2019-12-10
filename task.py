469ef536cdbf117c7546f41b004735fa070f09c0
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-12-10 09:06:29 -0400

    Train flow enhacements. Frame rendering, Reward computing ...

--- task.py
+++ task.py
@@ -1,18 +1,20 @@
 import numpy as np
-from control import put, on_game, tap
+from control import put, on_game, tap, transform_action
 import time
 from threading import Event
 from queue import Queue
 from android_screen import AndroidScreenBuffer
 import cv2
 from digit_recognition import DigitsMatcher
+from ounoise import OUNoise
 
 
 class Environment:
     """Environment, defines the goal and provides feedback to the agent."""
     def __init__(self, width=240, height=240, minicap_port=1313,
                  device_ref_elements_data={}):
-        self.asb = AndroidScreenBuffer(minicap_port=minicap_port)
+        self.asb = AndroidScreenBuffer(minicap_port=minicap_port,
+                                       scale_ratio=0.1, bitrate=120000)
         self.device_height, self.device_width = self.asb.get_device_screen_shape()
         self.action_repeat = 3
         self.state_frame_width = width
@@ -39,16 +41,18 @@ class Environment:
         time_importance = self.device_ref_elements_data['scores']['time_importance']
         y1, y2, x1, x2 = self.device_ref_elements_data['scores']['coords_diamonds_gathered']
         cropped_dig = state_frame[y1:y2, x1:x2]
-        diamonds_gathered = self.digits_matcher.match(state_frame,
+        cv2.imwrite(f'/tmp/frames/dig_{time.time()}.png', np.copy(cropped_dig))
+        diamonds_gathered = self.digits_matcher.match(cropped_dig,
                                                       threshold=match_threshold)
+        print(f"Diamonds Gathered: {diamonds_gathered}")
         if diamonds_gathered is None:
             #TODO: Enhance this
-            return -1
+            return 0
 
         return diamonds_importance * (diamonds_gathered/diamonds_total) - time_importance * t/episode_time_limit
 
 
-    def step(self, vector_size, angle, speed, show_frames=False):
+    def step(self, vector_size, angle, speed):
         """
             Uses action to obtain next state, reward, done.
             Through adb shell input applies a touchscreen swipe
@@ -62,21 +66,21 @@ class Environment:
 
         reward = 0
         states = []
+
         for _ in range(self.action_repeat):
             put(vector_size, angle, speed, self.device_width, self.device_height)
             t = time.time()
-            frame = None
-            while frame is None:
-                #frame = self.asb.get_timelapse_frame(t)
-                frame = self.asb.get_last_frame()
-            if show_frames and frame is not None:
-                cv2.imshow('frame', frame)
-            reward += self.get_reward(frame)
+            while True:
+                frame  = self.asb.get_last_frame()
+                if frame is not None:
+                    break
+
+            reward += self.get_reward(np.copy(frame))
             states.append(reward)
-            done = self.is_done(frame)
-        #next_state = np.concatenate(states)
+            done = self.is_done(np.copy(frame))
+
         next_state = []
-        return next_state, reward, done
+        return next_state, reward, done, np.copy(frame)
 
     def is_done(self, frame):
         '''
@@ -109,11 +113,11 @@ if __name__ == "__main__":
     }
 
     scores = {
-        'coords_diamonds_gathered': [11, 27, 24, 35],
+        'coords_diamonds_gathered': [11, 27, 25, 35],
         'digits_mask_addr': '/home/neo/dev/balloma_rl_agent/misc/digits',
         'match_threshold': 10,
         'time_importance': 0.7,
-        'diamond_importance': 0.3,
+        'diamonds_importance': 0.3,
         'episode_time_limit': 60,
         'diamonds_total': 7
 
@@ -122,13 +126,40 @@ if __name__ == "__main__":
     env = Environment(device_ref_elements_data={'try_again': try_again_el_data,
                                                 'scores': scores})
 
+    # Actions generation
+    exploration_mu = 0
+    exploration_theta = 0.15
+    exploration_sigma = 0.2
+    action_size = 3
+    action_low = np.array([1, 0, 1])
+    action_high = np.array([10, 359, 2000])
+    action_range = action_high - action_low
+
+    #Start with random action
+    action = np.array([np.random.uniform() for _ in action_low])
+    noise = OUNoise(action.shape[0], exploration_mu,
+                                 exploration_theta, exploration_sigma)
+
     for i in range(10):
         done = False
+        j = 0
         while not done:
-            angle = random.randint(0, 359)
-            v_size = random.randint(1, 50)
-            speed = random.randint(100, 2000)
-            ns, rw, done = env.step(v_size, angle, speed, show_frames=True)
+            j += 1
+            ns = noise.sample()
+            action = action + ns
+
+            #print(action)
+            #print(ns)
+            v_size, angle, speed = np.array(transform_action(action, action_range, action_low),
+                          dtype='uint8')
+            ns, rw, done, frame = env.step(v_size, angle, speed)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                #asb.stop()
+                exit(0)
+                break
+            #cv2.imwrite(f'/tmp/frames/frame_dig_{j}.png', frame[11:27, 25:35])
 
         env.reset()
 
