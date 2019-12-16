59bc069c05aa0968cd5ab7f41a8b04b517a88759
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-12-16 18:01:54 -0400

    Including action evolution in plots

--- plot_train.py
+++ plot_train.py
@@ -4,24 +4,26 @@ import pandas as pd
 import numpy as np
 
 def plot(file_addr):
-    fig, axes = plt.subplots(2)
-    
+    fig, axes = plt.subplots(5)
+
     while True:
         d = pd.read_csv(file_addr)
-        axes[0].set_ylabel('Reward')
-        axes[1].set_ylabel('Loss')
-        axes[0].grid()
-        axes[1].grid()
         x = [i for i in range(d.shape[0])]
-        axes[0].plot(x, d.reward)
-        reward_rolling = d.reward.rolling(100).mean()
-        axes[0].plot(np.arange(reward_rolling.shape[0]), reward_rolling)
-        axes[1].plot(x, d.loss)
-        loss_rolling = d.loss.rolling(100).mean()
-        axes[1].plot(np.arange(loss_rolling.shape[0]), loss_rolling)
+        v = ['reward', 'loss', 'vector_size', 'angle', 'speed']
+
+        for i, vv in enumerate(v):
+            axes[i].plot(x, d[vv])
+            rolling = d[vv].rolling(100).mean()
+            axes[i].plot(np.arange(rolling.shape[0]), rolling)
+            axes[i].set_ylabel(vv)
+            axes[i].grid()
+
+
         plt.pause(0.5)
-        axes[0].cla()
-        axes[1].cla()
+
+
+        for i, vv in enumerate(v):
+            axes[i].cla()
 
 if __name__ == "__main__":
     plot(sys.argv[1])
